1f3818178179d65e4b592e37907bd8516063577d
marte14 <kattymarte14@gmail.com>
2018-10-05 15:30:30 -0400

    uploading files into github, practice to share error free

---
+++ xcode-git/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  xcode-git
+//
+//  Created by katty y marte on 10/5/18.
+//  Copyright © 2018 katty y marte. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+//practice
